fc01982242aaaab3e94f7f8e99e498a008ec7123
meteoritosuchile <meteoritosuchile@gmail.com>
2026-06-11 00:40:27 -0400

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,3 +1,4 @@
+// Supabase configuration
 const SUPABASE_CONFIG = {
   url: "https://rldyvtztioxkypibbpju.supabase.co",
   anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJsZHl2dHp0aW94a3lwaWJicGp1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMTMwNzcsImV4cCI6MjA5NjY4OTA3N30.D5u43HPXG5KMCpk2P6dOejmHbikn8vqibX-Crh4CEnw"
